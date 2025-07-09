7289ff686ce69e6dbb70977fda805cbbbd74bbb5
scotland1912 <scotland1912@icloud.com>
2025-07-09 22:35:35 +0200

    Add Timer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    
+
+    public Timer timer;
     public float speed = 0;
     public TextMeshProUGUI scoreText;
     public GameObject winTextObject;
@@ -13,12 +14,14 @@ public class PlayerController : MonoBehaviour
     private int _score;
     private float _movementX;
     private float _movementY;
+    private bool _moved;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _score = 0;
+        _moved = false;
         
         SetScoreText();
         winTextObject.SetActive(false);
@@ -26,10 +29,15 @@ public class PlayerController : MonoBehaviour
 
     void OnMove(InputValue movementValue)
     {
+        if (!_moved)
+        {
+            timer.StartTimer();
+        }
         Vector2 movementVector = movementValue.Get<Vector2>();
         
         _movementX = movementVector.x;
         _movementY = movementVector.y;
+        _moved = true;
     }
 
     void SetScoreText()
@@ -37,6 +45,7 @@ public class PlayerController : MonoBehaviour
         scoreText.text = "Score: " + _score.ToString();
         if (_score >= 8)
         {
+            timer.StopTimer();
             winTextObject.SetActive(true);
         }
     }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    
+    public TextMeshProUGUI timerText;
+    private float _elapsedTime;
+    private bool _isRunning = false;
+    
+    public void StartTimer()
+    {
+        _elapsedTime = 0;
+        _isRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isRunning)
+        {
+            _elapsedTime += Time.deltaTime;
+            int minutes = (int)(_elapsedTime / 60);
+            int seconds = (int)(_elapsedTime % 60);
+            int milliseconds = (int)((_elapsedTime - (int)_elapsedTime) * 1000);
+
+            string formatted = string.Format("{0:D2}:{1:D2}:{2:D3}", minutes, seconds, milliseconds);
+            timerText.text = formatted;
+        }
+    }
+
+    public void StopTimer()
+    {
+        _isRunning = false;
+    }
+}
